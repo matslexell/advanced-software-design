ac2c9ba6e6d5b7fbc1d417ed938b1502e07aad1a
Mats Lexell <matslexell88@gmail.com>
2022-09-28 21:17:33 +0100

    wip - a bit messy, maybe throw away the alternating list thing

--- tic-tac-toe/TicTacToe.ts
+++ tic-tac-toe/TicTacToe.ts
@@ -1,49 +1,91 @@
 type NonEmptyArray<T> = [T, ...T[]];
 
 type Player = "X" | "O";
-const nextPlayer = (player: Player): Player => (player == "X" ? "O" : "X");
+type AlterSub<A, B> = [A] | [A, AlterSub<B, A> | [B]];
+type AlternatingList<A, B> = AlterSub<A, B> | AlterSub<B, A>;
 
-type XMove = {
-    player: "X",
-    pos: Pos
+function isFinishedGame(
+  game: FinishedGame | OngoingGame
+): game is FinishedGame {
+  return Math.random() > 0.5; // TODO implement
 }
 
-type OMove = {
-    player: "O",
-    pos: Pos
+function isEmptyGame(game: EmptyGame | OngoingGame): game is EmptyGame {
+  return game.moves == [];
 }
+
+function isOngoingGame(game: EmptyGame | OngoingGame): game is OngoingGame {
+  return game.moves != [];
+}
+
+function isXMove(move: XMove | OMove): move is XMove {
+  return move.player == "X";
+}
+
+function isOMove(move: XMove | OMove): move is OMove {
+  return move.player == "O";
+}
+
+// const alterBool: AlternatingList<true, false> = [true];
 
 type Pos = {
   col: 0 | 1 | 2;
   row: 0 | 1 | 2;
 };
 
-type Move = XMove | OMove;
+type Move = { pos: Pos };
 
-type Moves<T> = {
-  moves: T;
+type XMove = { player: "X" } & Move;
+type OMove = { player: "O" } & Move;
+
+type Moves = {
+  moves: AlternatingList<XMove, OMove>;
 };
 
-type EmptyGame = Moves<[]>;
+type EmptyGame = { moves: [] };
 
-type FinishedGame = Moves<NonEmptyArray<Move>>;
-
-type OngoingGame = Moves<NonEmptyArray<Move>>;
+type FinishedGame = Moves;
+type OngoingGame = Moves;
 
 type GameResult = `Player ${Player} wins` | "Game draw";
 
+const getNextMove = (
+  latestMove: XMove | OMove,
+  newMovePos: Pos
+): XMove | OMove => {
+  return { player: latestMove.player == "X" ? "O" : "X", pos: newMovePos };
+};
 
 const move =
   (game: EmptyGame | OngoingGame) =>
   (move: Move): OngoingGame | FinishedGame => {
-    return {};
+    if (isEmptyGame(game)) {
+    } else if (isOngoingGame(game)) {
+      const [latest, rest] = game.moves;
+
+      if (isXMove(latest)) {
+        const nextMove = getNextMove(latest, move.pos);
+        if (isOMove(nextMove)) {
+            return { moves: [nextMove, game.moves] };
+        }
+
+        return isOMove(nextMove) ? { moves: [nextMove, game.moves] };
+
+      } else if (isOMove(latest)) {
+        const nextMove = getNextMove(latest, move.pos);
+        return { moves: [nextMove, game.moves] };
+      }
+    }
   };
 
 const takeMoveBack = (
   game: OngoingGame | FinishedGame
 ): EmptyGame | OngoingGame => {
-  const derp: OngoingGame = { moves: [{ col: 2, row: 1 }] };
-  return { moves: [] };
+  const [latest, rest] = game.moves;
+  if (rest == undefined) {
+    return { moves: [] };
+  }
+  return { moves: rest };
 };
 
 const whoWonOrDraw = (game: FinishedGame): GameResult => {
