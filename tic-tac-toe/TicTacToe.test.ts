a9b6e335a7245ca741f8e90f3c55fe43c8c6d68e
Mats Lexell <matslexell88@gmail.com>
2022-09-29 17:56:15 +0100

    Added Jest

---
+++ tic-tac-toe/TicTacToe.test.ts
@@ -0,0 +1,145 @@
+import { isArgumentsObject } from "util/types";
+import {describe, expect, test} from '@jest/globals';
+
+import {
+  isGameFinished,
+  isOngoingGame,
+  EmptyGame,
+  FinishedGame,
+  OngoingGame,
+  move,
+  startNewGame,
+  isEmptyGame,
+  whoWonOrDraw,
+} from "./TicTacToe";
+
+type Pos = {
+  col: 0 | 1 | 2;
+  row: 0 | 1 | 2;
+};
+
+const game1: Pos[] = [
+  { col: 1, row: 1 },
+  { col: 1, row: 0 },
+  { col: 2, row: 2 },
+  { col: 1, row: 2 },
+  { col: 0, row: 0 },
+];
+
+const game2: Pos[] = [
+  { col: 0, row: 0 },
+  { col: 1, row: 0 },
+  { col: 0, row: 1 },
+  { col: 1, row: 1 },
+  { col: 2, row: 2 },
+  { col: 1, row: 2 },
+];
+
+const game3: Pos[] = [
+  { col: 0, row: 0 },
+  { col: 2, row: 0 },
+  { col: 2, row: 1 },
+  { col: 1, row: 0 },
+  { col: 0, row: 2 },
+  { col: 2, row: 2 },
+  { col: 1, row: 2 },
+  { col: 1, row: 1 },
+  { col: 0, row: 1 },
+];
+
+const game4: Pos[] = [
+  { col: 0, row: 0 },
+  { col: 2, row: 1 },
+  { col: 2, row: 2 },
+  { col: 1, row: 0 },
+  { col: 2, row: 0 },
+  { col: 0, row: 2 },
+  { col: 1, row: 2 },
+  { col: 1, row: 1 },
+  { col: 0, row: 1 },
+];
+
+const game5: Pos[] = [
+  { col: 2, row: 1 },
+  { col: 0, row: 2 },
+  { col: 0, row: 1 },
+  { col: 1, row: 1 },
+  { col: 2, row: 2 },
+  { col: 0, row: 1 },
+  { col: 1, row: 2 },
+  { col: 1, row: 1 },
+];
+
+// function that takes in a position array and fills a 3x3 matrix with the positions, alterning between values x and o
+const fillMatrixAndPrint = (posArray: Pos[]) => {
+  const matrix: string[][] = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
+  posArray.forEach((pos, index) => {
+    if (index % 2 === 0) {
+      matrix[pos.row][pos.col] = "x";
+    } else {
+      matrix[pos.row][pos.col] = "o";
+    }
+  });
+  matrix.forEach((row) => {
+    console.log(row);
+  });
+  console.log();
+  return matrix;
+};
+
+fillMatrixAndPrint(game1);
+fillMatrixAndPrint(game2);
+fillMatrixAndPrint(game3);
+fillMatrixAndPrint(game4);
+fillMatrixAndPrint(game5);
+
+const playGame = (positions: Pos[]): OngoingGame | FinishedGame | EmptyGame => {
+  let game: EmptyGame | OngoingGame | FinishedGame = startNewGame();
+  positions.forEach((pos) => {
+    if (!isGameFinished(game)) {
+      game = move(game)(pos);
+    }
+  });
+
+  return game;
+};
+
+describe("isGameFinished", () => {
+  it("Game 1", () => {
+    const game1Played = playGame(game1);
+    expect(isGameFinished(game1Played)).toBe(true);
+    if (isGameFinished(game1Played)) {
+      expect(whoWonOrDraw(game1Played)).toBe("X");
+    }
+  });
+  it("Game 2", () => {
+    const game2Played = playGame(game2);
+    expect(isGameFinished(game2Played)).toBe(true);
+    if (isGameFinished(game2Played)) {
+      expect(whoWonOrDraw(game2Played)).toBe("O");
+    }
+  });
+  it("Game 3", () => {
+    const game3Played = playGame(game3);
+    expect(isGameFinished(game3Played)).toBe(true);
+    if (isGameFinished(game3Played)) {
+      expect(whoWonOrDraw(game3Played)).toBe("X");
+    }
+  });
+  it("Game 4", () => {
+    const game4Played = playGame(game4);
+    expect(isGameFinished(game4Played)).toBe(true);
+    if (isGameFinished(game4Played)) {
+      expect(whoWonOrDraw(game4Played)).toBe("DRAW");
+    }
+  });
+  it("Game 5", () => {
+    const game5Played = playGame(game5);
+    expect(isGameFinished(game5Played)).toBe(false);
+    expect(isOngoingGame(game5Played)).toBe(true);
+  });
+});
--- tic-tac-toe/TicTacToe.ts
+++ tic-tac-toe/TicTacToe.ts
@@ -1,17 +1,25 @@
+const finishedGame = Symbol();
+const ongoingGame = Symbol();
+const emptygame = Symbol();
+
 type NonEmptyArray<T> = [T, ...T[]];
 
 function isGameFinished(
-  game: FinishedGame | OngoingGame
+  game: EmptyGame | OngoingGame | FinishedGame
 ): game is FinishedGame {
-  return Math.random() > 0.5; // TODO implement
+  return game.__type_proof == finishedGame;
 }
 
-function isEmptyGame(game: EmptyGame | OngoingGame): game is EmptyGame {
-  return game.moves == [];
+function isEmptyGame(game: EmptyGame | OngoingGame | FinishedGame): game is EmptyGame {
+  return game.__type_proof == emptygame;
 }
+
+function isOngoingGame(game: EmptyGame | OngoingGame | FinishedGame): game is EmptyGame {
+    return game.__type_proof == ongoingGame;
+  }
 
 function isPositionEqual(p1: Pos, p2: Pos) {
-    return p1.col == p2.col && p1.row == p2.row;
+  return p1.col == p2.col && p1.row == p2.row;
 }
 
 type Pos = {
@@ -19,7 +27,7 @@ type Pos = {
   row: 0 | 1 | 2;
 };
 
-type Move = {player: Player, pos: Pos}
+type Move = { player: Player; pos: Pos };
 
 type Moves<T> = {
   moves: T;
@@ -27,31 +35,46 @@ type Moves<T> = {
 
 type Player = "X" | "O";
 
-type EmptyGame = Moves<[]>;
-type FinishedGame = Moves<NonEmptyArray<Move>>;
-type OngoingGame = Moves<NonEmptyArray<Move>>;
+type EmptyGame = Moves<[]> & { __type_proof: typeof emptygame };
+type OngoingGame = Moves<NonEmptyArray<Move>> & {
+  __type_proof: typeof ongoingGame;
+};
+type FinishedGame = { result: GameResult } & Moves<NonEmptyArray<Move>> & {
+    __type_proof: typeof finishedGame;
+  };
 
-type GameResult = `Player ${Player} wins` | "Game draw";
+type GameResult = Player | "DRAW";
+
+const startNewGame = (): EmptyGame => {
+  return { moves: [], __type_proof: emptygame };
+};
 
 const move =
   (game: EmptyGame | OngoingGame) =>
-  (move: Move): OngoingGame | FinishedGame => {
-    isPositionOccupied()
-    
+  (pos: Pos): OngoingGame | FinishedGame => {
+    if (isEmptyGame(game)) {
+      return { moves: [{ player: "X", pos }], __type_proof: ongoingGame };
+    } else if (!isPositionOccupied(game)(pos)) {
+      const [head] = game.moves;
+      const player = head.player == "X" ? "O" : "X";
+      return { moves: [{ player, pos }, ...game.moves], __type_proof: ongoingGame };
+    } else {
+      return game;
+    }
   };
 
-const takeMoveBack = (
-  game: OngoingGame | FinishedGame
-): EmptyGame | OngoingGame => {
-  const [latest, rest] = game.moves;
-  if (rest == undefined) {
-    return { moves: [] };
-  }
-  return { moves: rest };
-};
+// const takeMoveBack = (
+//   game: OngoingGame | FinishedGame
+// ): EmptyGame | OngoingGame => {
+//   const [latest, rest] = game.moves;
+//   if (rest == undefined) {
+//     return { moves: [] };
+//   }
+//   return { moves: rest };
+// };
 
 const whoWonOrDraw = (game: FinishedGame): GameResult => {
-  return "Player O wins";
+  return "X";
 };
 
 const isPositionOccupied =
@@ -60,4 +83,15 @@ const isPositionOccupied =
     return game.moves.some((move) => isPositionEqual(move.pos, pos));
   };
 
-export { isGameFinished };
+export {
+  isGameFinished,
+  EmptyGame,
+  OngoingGame,
+  FinishedGame,
+  move,
+  startNewGame,
+  whoWonOrDraw,
+  isPositionOccupied,
+  isOngoingGame,
+  isEmptyGame,
+};
